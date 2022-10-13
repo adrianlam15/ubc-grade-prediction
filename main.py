14903261c0e29bdb159d21f6905805174c773d45
Adrian <adrianlam1511@gmail.com>
2022-10-13 00:50:06 -0700

    added linear regression, and prediction model

--- main.py
+++ main.py
@@ -33,12 +33,16 @@ class main:
 
     # SPECIFY CAMPUS AND COURSE CODE FUNCTION
     def specify(self):
-        CAMPUS = input("CAMPUS: ")
-        # input validation
-        while CAMPUS != "UBCV" and CAMPUS != "UBCO":
-            print("Invalid campus.")
+        if self.DEBUG_MODE:
+            CAMPUS = "UBCV"
+            COURSE_CODE = "CPSC 110"
+        else:
             CAMPUS = input("CAMPUS: ")
-        COURSE_CODE = input("COURSE CODE: ")
+            # input validation
+            while CAMPUS != "UBCV" and CAMPUS != "UBCO":
+                print("Invalid campus.")
+                CAMPUS = input("CAMPUS: ")
+            COURSE_CODE = input("COURSE CODE: ")
         return CAMPUS, COURSE_CODE
 
     # SETS CSV FILE GIVEN ARGS & READS CSV FILE FUNCTION
@@ -73,9 +77,11 @@ class main:
                 "Avg",
             ]
         ]
-        # for debugging purpoeses
+        # for debugging purposes
         if self.DEBUG_MODE:
-            print(self.df)
+            choice = input("Print course data? (y/n): ")
+            if choice == "y":
+                print(f"\n{self.df}")
 
     # DRAW FUNCTION
     def draw(self):
@@ -85,9 +91,8 @@ class main:
             y="Avg",
             kind="scatter",
         )
-        self.X = self.df["Year"]  # returns Year Series object
-        self.y = self.df.Avg
-        # plt.show()  # show plotted graph
+        plt.plot(self.X_TEST, self.avg_pred, color="red", linewidth=2)
+        plt.show()
 
     # STATISTICS FUNCTION
     def stats(self):
@@ -95,25 +100,42 @@ class main:
         self.TRAIN, self.TEST = train_test_split(
             self.df, test_size=0.3, random_state=SEED
         )
+        self.X = self.df["Year"]  # returns Year Series object
+        self.y = self.df.Avg
         # plt.show()
 
-    # change name later maybe
-    def predict(self):
-        regressor = LinearRegression()
+    # REGRESSION FUNCTION
+    def regression(self):
+        self.regressor = LinearRegression()
+
         self.X_TRAIN = self.TRAIN["Year"].values.reshape(-1, 1)
         self.Y_TRAIN = self.TRAIN["Avg"].values.reshape(-1, 1)
-        regressor.fit(self.X_TRAIN, self.Y_TRAIN)
-        print(regressor.intercept_)
-        print(regressor.coef_)
+        self.X_TEST = self.TEST["Year"].values.reshape(-1, 1)
+        self.Y_TEST = self.TEST["Avg"].values.reshape(-1, 1)
+
+        self.regressor.fit(self.X_TRAIN, self.Y_TRAIN)
+        self.avg_pred = self.regressor.predict(self.X_TEST)
+        if self.DEBUG_MODE:
+            df_preds = pd.DataFrame(
+                {"Actual": self.Y_TEST.squeeze(), "Predicted": self.avg_pred.squeeze()}
+            )
+            print(df_preds)
+
+    # PREDICT FUNCTION
+    def predict(self):
+        year = int(input("\nYear for prediction: "))
+        pred_avg = self.regressor.predict([[year]])
+        print(pred_avg.squeeze().round(2))
 
     # MAIN FUNCTION
     def main(self):
         CAMPUS, COURSE_CODE = self.specify()
         self.read(CAMPUS, COURSE_CODE)
         self.filter_df()
-        self.draw()
         self.stats()
+        self.regression()
         self.predict()
+        self.draw()
 
 
 # MAIN PROGRAM RUN ---------------------------------------------------------------------
